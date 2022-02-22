de36e7ac9fb525e3378a5831be29e60bc740f688
Husniddin01”git config --global user.email husniddinsadull55@gmail.comgit config --global user.name Husniddin01” <husniddinsadull55@gmail.com>
2022-02-22 17:51:26 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+// userName1="Husniddin"
+// userName2=456
+// userName3="500"
+// a=userName1+userName2+userName3
+// console.log(userName2*userName3);
+// console.log(a);
+// var a=43
+// var b=57
+// var c=(a+b)/(a-b)*10
+// console.log(c);
+// console.log(typeof "Husniddin","Husniddin");//string
+// console.log(typeof 7584326,7584326) //number
+// console.log(typeof true,true);//boolean
+// console.log(typeof false,false);//boolean
+// console.log(typeof undefined,undefined); //undefined
+// var salom
+// console.log( typeof salom, salom);
+// console.log(typeof typeof "salom");
+// console.log(typeof typeof 456567);
+// console.log(typeof undefined ,undefined);
+var hello= alert("Assalomu alaykum hurmatli foydalanuvchi")
+console.log(hello);
+var userName=prompt("Ismingizni kiriting")
+var userSname=prompt("Familiyangizni kiriting")
+var dadName=prompt("Otangizning ismi")
+var contry=prompt("Yashash joyingizni kiriting","O'zbekiston")
+var age=prompt("Yoshingizni kiriting","20")
+var telnamber=prompt("Telifon raqamingizni kiriting")
+var Userweight=prompt("Vazningizni kiriting")
+var Userheight=prompt("Bo'yingizni kiriting")
+var hobby=prompt("Hobbyingizni kiriting")
+var interisting=prompt("Qiziqishlaringizni kiriting")
+var information=("Assalomu alaykum hurmatli foydalanuvchi.\n" + "Ismingiz " + userName +".\n"+"Familiyangiz "+userSname+".\n"+"Otangizni ismi "+dadName+".\n"+"yashash joyingiz "+contry+".\n"+"yoshingiz "+age+".\n"+"telifon raqamingiz "+telnamber+".\n"+"Vazningiz "+Userweight+".\n"+"Bo'yingiz "+Userheight+".\n"+"Hobbyingiz "+hobby+".\n"+"Qiziqishlaringiz "+interisting+".")
+console.log(information);
+alert(information+"malumotlar notug'ri bo'lsa qayta toldiring")
+
+
